c81dc7fb05353a78f75cdc191e57aa8245266cfc
Ayesha <ayesha@gmail.com>
2026-01-12 02:24:05 +0500

    backend ready for vercel

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const cors = require("cors");
-
-
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
-// 👇 MUST
 app.use(express.json());
 app.use(cors());
 
-// 👇 routes import
+// routes
 const transactionRoutes = require("./routes/transactionroutes");
-
-// 👇 routes mount (MOST IMPORTANT)
 app.use("/api/transactions", transactionRoutes);
 
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
+// ❌ NO app.listen here
 mongoose
-  .connect("mongodb://127.0.0.1:27017/ftracker")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () => console.log("Server running on 5000"));
   })
   .catch((err) => console.log(err));
-const PORT = process.env.PORT || 5000;
 
 module.exports = app;
